c0d649f55e70a255d356ad5dcb92132b065f1a59
Julia Breitenbruch <119942939+JBreitenbr@users.noreply.github.com>
2024-11-24 18:59:25 +0000

    incorporated Card element

--- src/main.tsx
+++ src/main.tsx
@@ -5,9 +5,9 @@ import {QueryClient,QueryClientProvider} from '@tanstack/react-query'
 import 'semantic-ui-css/semantic.min.css'
 const queryClient = new QueryClient()
 ReactDOM.createRoot(document.getElementById('root')).render(
-	<React.StrictMode>
+	/*<React.StrictMode>*/
 		<QueryClientProvider client={queryClient}>
 			<App />
 		</QueryClientProvider>
-	</React.StrictMode>
+	/*</React.StrictMode>*/
 )
--- src/pages/home/column-display.tsx
+++ src/pages/home/column-display.tsx
@@ -1,3 +1,4 @@
+import {Grid,Card} from 'semantic-ui-react'
 import {DisplayType} from './index'
 interface DisplayData {
   id: number;
@@ -14,6 +15,9 @@ interface Props {
   displayType: DisplayType;
 }
 let rnd=Math.floor(Math.random()*10)
+
 export const ColumnDisplay = (props:Props) => {
-    return (<div>{props.displayType==DisplayType.Movies?props.data[rnd].title:props.data[rnd].name}</div>)
-  }
+const {data,displayType} = props
+let title=displayType==DisplayType.Movies?data[rnd].title:data[rnd].name;
+    return (<Grid columns={3} stackable centered verticalAlign="top" padded="vertically">{data.map((displayData:DisplayData)=><Grid.Column key={displayData.id}><Card.Group><Card fluid image={`https://image.tmdb.org/t/p/original/${displayData.poster_path}`} header={displayType==DisplayType.Movies?displayData.title:displayData.name} meta={displayType==DisplayType.Movies?`Release Date: ${displayData.release_date} | Rating: ${displayData.vote_average}`:`Rating: ${displayData.vote_average}`} description={displayData.overview.slice(0,350)+"..."}/></Card.Group></Grid.Column>)}</Grid>);
+  };
