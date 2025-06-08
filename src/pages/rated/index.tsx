7c8ab1f0619e3ecab1380f14e41714cf26ea036c
Julia Breitenbruch <119942939+JBreitenbr@users.noreply.github.com>
2025-06-08 13:05:08 +0200

    Update index.tsx

--- src/pages/rated/index.tsx
+++ src/pages/rated/index.tsx
@@ -16,5 +16,13 @@ const {data:ratedMovies,isLoading:isLoadingMovies} = useQuery({queryKey:["ratedM
 const {data:ratedTvShows,isLoading:isLoadingTvShows} = useQuery({queryKey:["ratedTvShows"],queryFn:fetchRatedTvShows})
   //document.write(JSON.stringify(ratedMovies))
   return (
-    <Container style={{marginTop:"80px"}}><Menu pointing secondary><Menu.Item name="Movies" active={activeTabs==DisplayType.Movies} onClick={()=>setActiveTabs(DisplayType.Movies)} /><Menu.Item name="TV Shows" active={activeTabs==DisplayType.TvShows} onClick={()=>setActiveTabs(DisplayType.TvShows)} /></Menu><Segment>{isLoadingMovies||isLoadingTvShows?(<div>Loading...</div>):(<div style={{marginTop:20}}>{displayType==DisplayType.Movies?(<ColumnDisplay data={ratedMovies.results} displayType={DisplayType.Movies}/>):(<ColumnDisplay data={ratedTvShows.results} displayType={DisplayType.TvShows}/>)}</div>)}</Segment></Container>)
+    <Container style={{marginTop:"80px"}}><Menu pointing secondary>
+        <Menu.Item name="Movies" active={activeTabs==DisplayType.Movies} onClick={()=>setActiveTabs(DisplayType.Movies)} />
+        <Menu.Item name="TV Shows" active={activeTabs==DisplayType.TvShows} onClick={()=>setActiveTabs(DisplayType.TvShows)} />
+    </Menu>
+        <Segment>{isLoadingMovies||isLoadingTvShows?(<div>Loading...</div>):(<div style={{marginTop:20}}>
+            {displayType==DisplayType.Movies?(
+        <ColumnDisplay data={ratedMovies.results} displayType={DisplayType.Movies}/>):
+            (<ColumnDisplay data={ratedTvShows.results} displayType={DisplayType.TvShows}/>)}</div>)}</Segment>
+    </Container>)
 }
